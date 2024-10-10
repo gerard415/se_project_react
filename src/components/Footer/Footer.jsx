faa329e901eccbf1b635ed9394cf2bcce66cfedc
gerard415 <chinedumike85@gmail.com>
2024-10-09 21:38:17 -0400

    Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,12 @@
-import './Footer.css'
+import "./Footer.css";
 
 function Footer() {
-    return (
-      <div>
-        
-      </div>
-  )
+  return (
+    <footer className="footer">
+      <p className="footer__copyright">Developed by Gerard Ike</p>
+      <p className="footer__year">2024</p>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
