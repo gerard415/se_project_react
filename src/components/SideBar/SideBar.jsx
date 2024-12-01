1291e5ec3bc5446f70d02f0bcc734d2a70d3ef1c
gerard415 <chinedumike85@gmail.com>
2024-11-30 20:52:25 -0500

    sidebar component

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,11 +1,22 @@
 import "./SideBar.css";
 import avatar from "../../assets/avatar.svg";
+import { useContext } from "react";
+import { CurrentUserContext } from "../../contexts/CurrentUserContext";
 
-function SideBar() {
+function SideBar({ onSignOut, handleEditProfileClick }) {
+  const currentUser = useContext(CurrentUserContext);
   return (
     <div className="sidebar">
-      <img className="sidebar__avatar" src={avatar} alt="profile image" />
-      <p className="sidebar__username">Terrence Tegegne</p>
+      <img className="sidebar__avatar" src={!currentUser ? avatar : currentUser.avatar} alt="profile image" />
+      <p className="sidebar__username">{!currentUser ? "" : currentUser.name}</p>
+      <div className="sidebar__user">
+        <button onClick={handleEditProfileClick} className="sidebar__info">
+          Change Profile Data
+        </button>
+        <button onClick={onSignOut} className="sidebar__logout">
+          Log Out
+        </button>
+      </div>
     </div>
   );
 }
