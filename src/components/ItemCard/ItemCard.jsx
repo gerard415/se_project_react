5866e9de0224dd67a6ebc64ce12ee1e771bee1cd
gerard415 <chinedumike85@gmail.com>
2024-11-30 20:53:53 -0500

    Item Card component

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -1,6 +1,15 @@
 import "./ItemCard.css";
+import { useContext } from "react";
+import { CurrentUserContext } from "../../contexts/CurrentUserContext";
 
-function ItemCard({ item, onCardClick }) {
+function ItemCard({ item, onCardClick, onToggleLike, isLoggedIn, likedItems, }) {
+  const currentUser = useContext(CurrentUserContext);
+  const isLiked = item.likes.some((id) => id === currentUser._id);
+
+  const handleLike = () => {
+    onToggleLike(item._id, isLiked);
+  };
+
   const handleCardClick = () => {
     onCardClick(item);
   };
@@ -8,6 +17,12 @@ function ItemCard({ item, onCardClick }) {
   return (
     <li className="card">
       <h2 className="card__name">{item.name}</h2>
+      {isLoggedIn && (
+        <button
+          onClick={handleLike}
+          className={`card__like-btn ${isLiked ? "card__like-btn_liked" : ""}`}
+        ></button>
+      )}
       <img
         onClick={handleCardClick}
         className="card__image"
