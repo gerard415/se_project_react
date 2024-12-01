67ae82505a2c1a8a2ae2847bfe9df8c65026a2f3
gerard415 <chinedumike85@gmail.com>
2024-11-30 20:51:33 -0500

    Footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="footer">
       <ul className="footer__content">
-        <li className="footer__content-name">Developed by Prince Raval</li>
+        <li className="footer__content-name">Developed by Gerard Ike</li>
         <li className="footer__content-year"> 2024</li>
       </ul>
     </footer>
