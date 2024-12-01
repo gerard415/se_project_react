29f194dff8d0eb6043542817f7bf8b7ec8b5ae50
gerard415 <chinedumike85@gmail.com>
2024-11-30 20:50:33 -0500

    Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,13 +2,18 @@ import "./Header.css";
 import logo from "../../assets/logo.svg";
 import avatar from "../../assets/avatar.svg";
 import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { CurrentUserContext } from "../../contexts/CurrentUserContext";
 
-function Header({ handleAddClick, weatherData }) {
+function Header({ handleAddClick, weatherData, handleSignUpClick, handleLoginClick, isLoggedIn }) {
+  const currentUser = useContext(CurrentUserContext);
   const currentDate = new Date().toLocaleString("default", {
     month: "long",
     day: "numeric",
   });
+
+  if (isLoggedIn === true) {
   return (
     <header className="header">
       <Link to="/">
@@ -28,12 +33,31 @@ function Header({ handleAddClick, weatherData }) {
       </button>
       <Link to="/profile" className="header__link">
         <div className="header__user">
-          <p className="header__username">Terrence Tegegne</p>
-          <img src={avatar} alt="Terrence Tegegne" className="header__avatar" />
+          <p className="header__username">{currentUser.name}</p>
+          <img src={currentUser.avatar} alt="Terrence Tegegne" className="header__avatar" />
         </div>
       </Link>
     </header>
-  );
+  )}else {
+    return (
+      <header className="header">
+        <Link to="/">
+          <img className="header__logo" src={logo} alt="logo" />
+        </Link>
+
+        <p className="header__date-and-location">
+          {currentDate}, {weatherData.city}
+        </p>
+        <ToggleSwitch />
+        <button onClick={handleSignUpClick} className="header__signup">
+          Sign Up
+        </button>
+        <button onClick={handleLoginClick} className="header__login">
+          Log In
+        </button>
+      </header>
+    );
+  }
 }
 
 export default Header;
