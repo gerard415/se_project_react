54bedb9a5eaa944af9436e1cd940fb4bdfc05934
gerard415 <chinedumike85@gmail.com>
2024-11-30 20:48:29 -0500

    like and unlike card api calls

--- src/utils/api.js
+++ src/utils/api.js
@@ -7,12 +7,13 @@ export function checkResponse(res) {
   return Promise.reject(`Error: ${res.status}`);
 }
 
-const baseUrl = "http://localhost:3001";
+export const baseUrl = "http://localhost:3001";
 export const getItems = () => {
   return fetch(`${baseUrl}/items`).then(checkResponse);
 };
 
 export const postItem = (name, link, weather) => {
+  const token = localStorage.getItem("jwt");
   return fetch(`${baseUrl}/items`, {
     method: "POST",
     body: JSON.stringify({
@@ -22,16 +23,38 @@ export const postItem = (name, link, weather) => {
     }),
     headers: {
       "Content-Type": "application/json; charset=UTF-8",
+      authorization: `Bearer ${token}`,
     },
   }).then(checkResponse);
 };
 
 export const deleteItem = (id) => {
+  const token = localStorage.getItem("jwt");
   return fetch(`${baseUrl}/items/${id}`, {
     method: "DELETE",
-
     headers: {
       "Content-Type": "application/json; charset=UTF-8",
+      authorization: `Bearer ${token}`,
     },
   }).then(checkResponse);
 };
+
+export const addCardLike = (id, token) => {
+  return fetch(`${baseUrl}/items/${id}/likes`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      authorization: `Bearer ${token}`,
+    },
+  }).then((res) => checkResponse(res));
+}
+
+export const removeCardLike = (id, token) => {
+  return fetch(`${baseUrl}/items/${id}/likes`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      authorization: `Bearer ${token}`,
+    },
+  }).then((res) => checkResponse(res));
+}
